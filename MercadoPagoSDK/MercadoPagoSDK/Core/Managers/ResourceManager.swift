3fa86acafd4e06ce3df4de606f336e2b993872df
Juan Sanzone <juan.sanzone@gmail.com>
2019-05-29 18:35:30 -0300

    Align with Android, waiting for payment will be green

--- MercadoPagoSDK/MercadoPagoSDK/Core/Managers/ResourceManager.swift
+++ MercadoPagoSDK/MercadoPagoSDK/Core/Managers/ResourceManager.swift
@@ -190,7 +190,7 @@ extension ResourceManager {
         if let statusDetail = statusDetail {
             //Payment Result Logic
             let paymentResult = PaymentResult(status: status, statusDetail: statusDetail, paymentData: PXPaymentData(), splitAccountMoney: nil, payerEmail: nil, paymentId: nil, statementDescription: nil)
-            if paymentResult.isApproved() {
+            if paymentResult.isApproved() || paymentResult.isWaitingForPayment() {
                 return ThemeManager.shared.successColor()
             }
             if paymentResult.isContingency() || paymentResult.isReviewManual() || paymentResult.isWarning() {
