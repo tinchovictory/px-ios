1f52aaca76d9bbbeed02a88e09752c1100ef0a67
Federico Bustos Fierro <federico0812@gmail.com>
2019-05-31 14:32:07 -0300

    private fields

--- MercadoPagoSDK/MercadoPagoSDK/Services/MercadoPagoServices.swift
+++ MercadoPagoSDK/MercadoPagoSDK/Services/MercadoPagoServices.swift
@@ -12,8 +12,8 @@ internal class MercadoPagoServices: NSObject {
 
     open var merchantPublicKey: String
     open var payerAccessToken: String
-    open var processingModes: [String]
-    open var branchId: String?
+    private var processingModes: [String]
+    private var branchId: String?
     private var baseURL: String! = PXServicesURLConfigs.MP_API_BASE_URL
     private var gatewayBaseURL: String!
 
