6adf0c7ff3cb023b728c4b1e40a998bdb403faf4
Juan Sanzone <juan.sanzone@gmail.com>
2019-07-23 14:14:44 -0300

    Improvement code

--- MercadoPagoSDK/MercadoPagoSDK/Core/Utils.swift
+++ MercadoPagoSDK/MercadoPagoSDK/Core/Utils.swift
@@ -234,16 +234,13 @@ internal class Utils {
 
         return stringToWrite
     }
+
     class func getFont(size: CGFloat) -> UIFont {
         return getFontWithSize(font: ThemeManager.shared.getFontName(), size: size)
     }
 
     class func getLightFont(size: CGFloat) -> UIFont {
-        if #available(iOS 8.2, *) {
-            return UIFont(name: ThemeManager.shared.getLightFontName(), size: size) ?? UIFont.systemFont(ofSize: size, weight: UIFont.Weight.thin)
-        } else {
-            return UIFont(name: ThemeManager.shared.getLightFontName(), size: size) ?? UIFont.systemFont(ofSize: size)
-        }
+        return UIFont(name: ThemeManager.shared.getLightFontName(), size: size) ?? getFallbackFont(size, weight: UIFont.Weight.thin)
     }
 
     class func getSemiBoldFont(size: CGFloat) -> UIFont {
@@ -255,21 +252,21 @@ internal class Utils {
             if thisFont.familyName != "Times New Roman" {
                 return thisFont
             } else {
-                return UIFont(name: "Helvetica", size: size) ?? UIFont.systemFont(ofSize: size)
+                return UIFont(name: "Helvetica", size: size) ?? getFallbackFont(size)
             }
-        } else if #available(iOS 8.2, *), let thisWeight = weight {
-            return UIFont.systemFont(ofSize: size, weight: thisWeight)
-        } else {
-            return UIFont.systemFont(ofSize: size)
         }
+        return getFallbackFont(size)
+    }
+
+    private class func getFallbackFont(_ size: CGFloat, weight: UIFont.Weight?=nil) -> UIFont {
+        if let targetWeight = weight {
+            return UIFont.systemFont(ofSize: size, weight: targetWeight)
+        }
+        return UIFont.systemFont(ofSize: size)
     }
 
     class func getIdentificationFont(size: CGFloat) -> UIFont {
-        if #available(iOS 8.2, *) {
-            return UIFont(name: "KohinoorBangla-Regular", size: size) ?? UIFont.systemFont(ofSize: size, weight: UIFont.Weight.thin)
-        } else {
-            return UIFont(name: "KohinoorBangla-Regular", size: size) ?? UIFont.systemFont(ofSize: size)
-        }
+        return UIFont(name: "KohinoorBangla-Regular", size: size) ?? UIFont.systemFont(ofSize: size)
     }
 
     class func append(firstJSON: String, secondJSON: String) -> String {
