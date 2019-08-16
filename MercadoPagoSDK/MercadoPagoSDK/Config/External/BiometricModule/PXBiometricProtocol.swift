92ebe63bf39393c1d661120ec1d609dd879d8916
Federico Ventre <federico.ventre@mercadolibre.com>
2019-08-16 11:44:17 -0300

    add @escaping

--- MercadoPagoSDK/MercadoPagoSDK/Config/External/BiometricModule/PXBiometricProtocol.swift
+++ MercadoPagoSDK/MercadoPagoSDK/Config/External/BiometricModule/PXBiometricProtocol.swift
@@ -11,6 +11,6 @@ import Foundation
  Use this protocol to implement Biometric validation
  */
 @objc public protocol PXBiometricProtocol: NSObjectProtocol {
-    func validate(config: PXBiometricConfig, onSuccess: @escaping () -> Void, onError: (Error) -> Void)
+    func validate(config: PXBiometricConfig, onSuccess: @escaping () -> Void, @escaping onError: (Error) -> Void)
     func isValidationRequired(config: PXBiometricConfig) -> Bool
 }
