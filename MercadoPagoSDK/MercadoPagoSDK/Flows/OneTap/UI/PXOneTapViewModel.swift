90edd2c051288e45e782e4ff531c08841058a5f6
augustocollerone <collerone.augusto@gmail.com>
2019-11-25 14:36:45 -0300

    Primary message color forced

--- MercadoPagoSDK/MercadoPagoSDK/Flows/OneTap/UI/PXOneTapViewModel.swift
+++ MercadoPagoSDK/MercadoPagoSDK/Flows/OneTap/UI/PXOneTapViewModel.swift
@@ -136,7 +136,8 @@ extension PXOneTapViewModel {
             let selectedPayerCost = sliderNode.selectedPayerCost
             let installment = PXInstallment(issuer: nil, payerCosts: payerCost, paymentMethodId: nil, paymentTypeId: nil)
 
-            if !sliderNode.status.enabled, let disabledMessage = sliderNode.status.mainMessage?.getAttributedString(fontSize: installmentsRowMessageFontSize) {
+            let disabledMessage = sliderNode.status.mainMessage?.getAttributedString(fontSize: installmentsRowMessageFontSize, textColor: ThemeManager.shared.getAccentColor())
+            if !sliderNode.status.enabled, let disabledMessage = disabledMessage {
                 let disabledInfoModel = PXOneTapInstallmentInfoViewModel(text: disabledMessage,
                                                                          installmentData: nil,
                                                                          selectedPayerCost: nil,
