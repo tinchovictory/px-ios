0439d674e68f9aadcc65673e491471b9873699dc
Esteban Adrian Boffa <esteban.boffa@mercadolibre.com>
2020-02-04 23:54:51 -0300

    CreditsExpectationView improvements

--- MercadoPagoSDK/MercadoPagoSDK/UI/CreditsExpectation/PXCreditsExpectationView.swift
+++ MercadoPagoSDK/MercadoPagoSDK/UI/CreditsExpectation/PXCreditsExpectationView.swift
@@ -10,12 +10,14 @@ import Foundation
 
 final class PXCreditsExpectationView: PXBodyView {
 
+    // Constants
     let TITLE_FONT_SIZE: CGFloat = PXLayout.XS_FONT
     let SUBTITLE_FONT_SIZE: CGFloat = PXLayout.XXS_FONT
+    let props: PXCreditsExpectationProps
 
+    // Variables
     var titleLabel: UILabel?
     var subtitleLabel: UILabel?
-    let props: PXCreditsExpectationProps
 
     init(props: PXCreditsExpectationProps) {
         self.props = props
@@ -26,44 +28,56 @@ final class PXCreditsExpectationView: PXBodyView {
     required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
+}
 
+// MARK: Privates
+private extension PXCreditsExpectationView {
     func createSubviews() {
-        self.backgroundColor = .white
-        self.translatesAutoresizingMaskIntoConstraints = false
+        backgroundColor = .white
+        translatesAutoresizingMaskIntoConstraints = false
 
         // Title
-        let title = UILabel()
-        title.translatesAutoresizingMaskIntoConstraints = false
-        self.titleLabel = title
-        self.addSubview(title)
-        title.font = UIFont.ml_regularSystemFont(ofSize: TITLE_FONT_SIZE)
-        title.text = props.title
-        title.textColor = UIColor(red: 0, green: 0, blue: 0, alpha: 0.8)
-        title.textAlignment = .left
-        title.numberOfLines = 0
-        self.pinFirstSubviewToTop(withMargin: PXLayout.ZERO_MARGIN)?.isActive = true
-        PXLayout.matchWidth(ofView: title, toView: self, withPercentage: CGFloat(80), relation: .equal).isActive = true
+        let title = buildTitle()
+        addSubview(title)
+        pinFirstSubviewToTop(withMargin: PXLayout.ZERO_MARGIN)?.isActive = true
         NSLayoutConstraint.activate([
             title.leadingAnchor.constraint(equalTo: self.leadingAnchor),
             title.trailingAnchor.constraint(equalTo: self.trailingAnchor)
         ])
 
-        //Subtitle
-        let detailLabel = UILabel()
-        detailLabel.numberOfLines = 0
-        detailLabel.translatesAutoresizingMaskIntoConstraints = false
-        self.addSubview(detailLabel)
-        self.subtitleLabel = detailLabel
-        detailLabel.font = UIFont.ml_regularSystemFont(ofSize: SUBTITLE_FONT_SIZE)
-        detailLabel.text = props.subtitle
-        detailLabel.textColor = UIColor(red: 0, green: 0, blue: 0, alpha: 0.45)
-        detailLabel.textAlignment = .left
-        self.putOnBottomOfLastView(view: detailLabel, withMargin: PXLayout.XXXS_MARGIN)?.isActive = true
-        PXLayout.matchWidth(ofView: detailLabel, toView: self, withPercentage: CGFloat(80), relation: .equal).isActive = true
+        // Subtitle
+        let detailLabel = buildSubtitle()
+        addSubview(detailLabel)
+        putOnBottomOfLastView(view: detailLabel, withMargin: PXLayout.XXXS_MARGIN)?.isActive = true
         NSLayoutConstraint.activate([
             detailLabel.leadingAnchor.constraint(equalTo: self.leadingAnchor),
             detailLabel.trailingAnchor.constraint(equalTo: self.trailingAnchor)
         ])
-        self.pinLastSubviewToBottom(withMargin: PXLayout.ZERO_MARGIN)?.isActive = true
+
+        pinLastSubviewToBottom(withMargin: PXLayout.ZERO_MARGIN)?.isActive = true
+    }
+
+    func buildTitle() -> UILabel {
+        let title = UILabel()
+        title.translatesAutoresizingMaskIntoConstraints = false
+        titleLabel = title
+        title.font = UIFont.ml_regularSystemFont(ofSize: TITLE_FONT_SIZE)
+        title.text = props.title
+        title.textColor = UIColor(red: 0, green: 0, blue: 0, alpha: 0.8)
+        title.textAlignment = .left
+        title.numberOfLines = 0
+        return title
+    }
+
+    func buildSubtitle() -> UILabel {
+        let detailLabel = UILabel()
+        detailLabel.numberOfLines = 0
+        detailLabel.translatesAutoresizingMaskIntoConstraints = false
+        subtitleLabel = detailLabel
+        detailLabel.font = UIFont.ml_regularSystemFont(ofSize: SUBTITLE_FONT_SIZE)
+        detailLabel.text = props.subtitle
+        detailLabel.textColor = UIColor(red: 0, green: 0, blue: 0, alpha: 0.45)
+        detailLabel.textAlignment = .left
+        return detailLabel
     }
 }
