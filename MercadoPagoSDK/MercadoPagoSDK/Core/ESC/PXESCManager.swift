bde16ecdfe95d925bab7b4c3af444608b298ca4d
Augusto Collerone <augusto.collerone@mercadolibre.com>
2020-03-06 13:54:28 -0300

    Feature - ESC reason tracking enabled (#2164)

--- MercadoPagoSDK/MercadoPagoSDK/Core/ESC/PXESCManager.swift
+++ MercadoPagoSDK/MercadoPagoSDK/Core/ESC/PXESCManager.swift
@@ -86,9 +86,8 @@ internal class PXESCManager: NSObject, MercadoPagoESC {
     func deleteESC(cardId: String, reason: PXESCDeleteReason, detail: String?) {
         if hasESCEnable() {
             #if PX_PRIVATE_POD
-//                let deleteESCReason = MLESCDeleteReason(rawValue: reason.rawValue)
-                mLESCManager.deleteESC(cardId: cardId)
-//                mLESCManager.deleteESC(cardId: cardId, reason: deleteESCReason, detail: detail)
+                let deleteESCReason = MLESCDeleteReason(rawValue: reason.rawValue)
+                mLESCManager.deleteESC(cardId: cardId, reason: deleteESCReason, detail: detail)
             #endif
         }
     }
@@ -96,9 +95,8 @@ internal class PXESCManager: NSObject, MercadoPagoESC {
     func deleteESC(firstSixDigits: String, lastFourDigits: String, reason: PXESCDeleteReason, detail: String?) {
         if hasESCEnable() {
             #if PX_PRIVATE_POD
-//            let deleteESCReason = MLESCDeleteReason(rawValue: reason.rawValue)
-            mLESCManager.deleteESC(firstSixDigits: firstSixDigits, lastFourDigits: lastFourDigits)
-//            mLESCManager.deleteESC(firstSixDigits: firstSixDigits, lastFourDigits: lastFourDigits, reason: deleteESCReason, detail: detail)
+            let deleteESCReason = MLESCDeleteReason(rawValue: reason.rawValue)
+            mLESCManager.deleteESC(firstSixDigits: firstSixDigits, lastFourDigits: lastFourDigits, reason: deleteESCReason, detail: detail)
             #endif
         }
     }
@@ -106,13 +104,11 @@ internal class PXESCManager: NSObject, MercadoPagoESC {
     func deleteESC(token: PXToken, reason: PXESCDeleteReason, detail: String?) {
         if hasESCEnable() {
             #if PX_PRIVATE_POD
-//            let deleteESCReason = MLESCDeleteReason(rawValue: reason.rawValue)
+            let deleteESCReason = MLESCDeleteReason(rawValue: reason.rawValue)
             if token.hasCardId() {
-                mLESCManager.deleteESC(cardId: token.cardId)
-//                mLESCManager.deleteESC(cardId: token.cardId, reason: deleteESCReason, detail: detail)
+                mLESCManager.deleteESC(cardId: token.cardId, reason: deleteESCReason, detail: detail)
             } else {
-                mLESCManager.deleteESC(firstSixDigits: token.firstSixDigits, lastFourDigits: token.lastFourDigits)
-//                mLESCManager.deleteESC(firstSixDigits: token.firstSixDigits, lastFourDigits: token.lastFourDigits, reason: deleteESCReason, detail: detail)
+                mLESCManager.deleteESC(firstSixDigits: token.firstSixDigits, lastFourDigits: token.lastFourDigits, reason: deleteESCReason, detail: detail)
             }
             #endif
         }
