0b0b98b37acf08e5aa8aa1aecfa1593eabc4df97
Esteban Adrian Boffa <esteban.boffa@mercadolibre.com>
2020-05-12 10:35:49 -0300

    Make imageUrl optional

--- MercadoPagoSDK/MercadoPagoSDK/Models/PXCVVInfo.swift
+++ MercadoPagoSDK/MercadoPagoSDK/Models/PXCVVInfo.swift
@@ -11,7 +11,7 @@ public struct PXCVVInfo: Codable {
 
     let title: String
     let message: String?
-    let imageUrl: String
+    let imageUrl: String?
 
     enum CodingKeys: String, CodingKey {
         case title
