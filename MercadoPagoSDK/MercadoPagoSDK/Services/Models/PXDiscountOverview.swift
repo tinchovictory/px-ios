cff8332cb416bb2900736b8792bf4620a344a390
Esteban Adrian Boffa <esteban.boffa@mercadolibre.com>
2020-07-02 11:17:22 -0300

    Update dto name

---
+++ MercadoPagoSDK/MercadoPagoSDK/Services/Models/PXDiscountOverview.swift
@@ -0,0 +1,24 @@
+//
+//  PXOverview.swift
+//  MercadoPagoSDK
+//
+//  Created by Esteban Adrian Boffa on 01/07/2020.
+//
+
+import Foundation
+
+public struct PXOverview: Codable {
+    let title: PXText
+    let subtitle: PXText?
+    let amount: PXText
+    let description: PXText?
+    let iconUrl: String?
+
+    enum CodingKeys: String, CodingKey {
+        case title
+        case subtitle
+        case amount
+        case description
+        case iconUrl = "icon_url"
+    }
+}
