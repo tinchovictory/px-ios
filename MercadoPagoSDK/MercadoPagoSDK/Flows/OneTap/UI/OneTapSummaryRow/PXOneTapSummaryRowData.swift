fb31156598893ac2421c5aeedffcffa271c16859
Esteban Adrian Boffa <esteban.boffa@mercadolibre.com>
2020-07-02 12:01:08 -0300

    Add overview property

--- MercadoPagoSDK/MercadoPagoSDK/Flows/OneTap/UI/OneTapSummaryRow/PXOneTapSummaryRowData.swift
+++ MercadoPagoSDK/MercadoPagoSDK/Flows/OneTap/UI/OneTapSummaryRow/PXOneTapSummaryRowData.swift
@@ -15,8 +15,9 @@ class PXOneTapSummaryRowData: Equatable {
     let isTotal: Bool
     let image: UIImage?
     let type: PXOneTapSummaryRowView.RowType?
+    let overview: PXOverview?
 
-    init(title: String, value: String, highlightedColor: UIColor, alpha: CGFloat, isTotal: Bool, image: UIImage?, type: PXOneTapSummaryRowView.RowType?) {
+    init(title: String, value: String, highlightedColor: UIColor, alpha: CGFloat, isTotal: Bool, image: UIImage?, type: PXOneTapSummaryRowView.RowType?, overview: PXOverview? = nil) {
         self.title = title
         self.value = value
         self.highlightedColor = highlightedColor
@@ -24,9 +25,10 @@ class PXOneTapSummaryRowData: Equatable {
         self.isTotal = isTotal
         self.image = image
         self.type = type
+        self.overview = overview
     }
 
     static func == (lhs: PXOneTapSummaryRowData, rhs: PXOneTapSummaryRowData) -> Bool {
-        return lhs.title == rhs.title && lhs.value == rhs.value && lhs.highlightedColor == rhs.highlightedColor && lhs.alpha == rhs.alpha && lhs.isTotal == rhs.isTotal && lhs.image == rhs.image && lhs.type == rhs.type
+        return lhs.title == rhs.title && lhs.value == rhs.value && lhs.highlightedColor == rhs.highlightedColor && lhs.alpha == rhs.alpha && lhs.isTotal == rhs.isTotal && lhs.image == rhs.image && lhs.type == rhs.type && lhs.overview == rhs.overview
     }
 }
