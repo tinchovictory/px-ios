7907379ae68e04f65670e874be56569a1e44dfe8
Esteban Adrian Boffa <esteban.boffa@mercadolibre.com>
2020-07-08 11:59:09 -0300

    Add brief color and remove extension

--- MercadoPagoSDK/MercadoPagoSDK/Flows/OneTap/UI/OneTapSummaryRow/PXOneTapSummaryRowData.swift
+++ MercadoPagoSDK/MercadoPagoSDK/Flows/OneTap/UI/OneTapSummaryRow/PXOneTapSummaryRowData.swift
@@ -16,9 +16,10 @@ class PXOneTapSummaryRowData: Equatable {
     let image: UIImage?
     let type: PXOneTapSummaryRowView.RowType?
     let overview: PXOverview?
+    let briefColor: UIColor?
     var splitMoney = false
 
-    init(title: String, value: String, highlightedColor: UIColor, alpha: CGFloat, isTotal: Bool, image: UIImage?, type: PXOneTapSummaryRowView.RowType?, overview: PXOverview? = nil) {
+    init(title: String, value: String, highlightedColor: UIColor, alpha: CGFloat, isTotal: Bool, image: UIImage?, type: PXOneTapSummaryRowView.RowType?, overview: PXOverview? = nil, briefColor: UIColor? = nil) {
         self.title = title
         self.value = value
         self.highlightedColor = highlightedColor
@@ -27,6 +28,7 @@ class PXOneTapSummaryRowData: Equatable {
         self.image = image
         self.type = type
         self.overview = overview
+        self.briefColor = briefColor
     }
 
     static func == (lhs: PXOneTapSummaryRowData, rhs: PXOneTapSummaryRowData) -> Bool {
@@ -52,27 +54,23 @@ extension PXOneTapSummaryRowData {
 
     func getDescriptionText() -> NSAttributedString? {
         guard let description = overview?.description else { return nil }
-        return getAttributedText(array: description, fontSize: PXLayout.XXS_FONT)
+        return getAttributedText(array: description, textColor: highlightedColor, fontSize: PXLayout.XXS_FONT)
     }
 
     func getBriefText() -> NSAttributedString? {
         guard let brief = overview?.brief else { return nil }
-        return getAttributedText(array: brief, fontSize: PXLayout.XXXS_FONT)
+        return getAttributedText(array: brief, textColor: briefColor, fontSize: PXLayout.XXXS_FONT)
     }
 
     func getIconUrl() -> String? {
         return overview?.iconUrl
     }
 
-    private func getAttributedText(array: [PXText], fontSize: CGFloat) -> NSAttributedString {
-        var attributedString = NSAttributedString()
+    private func getAttributedText(array: [PXText], textColor: UIColor?, fontSize: CGFloat) -> NSAttributedString {
+        let attributedString = NSMutableAttributedString()
         for (index, text) in array.enumerated() {
-            if let attrString = text.getAttributedString(fontSize: fontSize, textColor: highlightedColor, backgroundColor: .clear) {
-                if index > 0 {
-                    attributedString = attributedString + NSAttributedString(string: " ") + attrString
-                } else {
-                    attributedString = attributedString + attrString
-                }
+            if let attrString = text.getAttributedString(fontSize: fontSize, textColor: textColor, backgroundColor: .clear) {
+                index == 0 ? attributedString.append(attrString) : attributedString.appendWithSpace(attrString)
             }
         }
         return attributedString
