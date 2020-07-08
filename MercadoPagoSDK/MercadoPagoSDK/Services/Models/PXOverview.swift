6733f0d4ff007ec41a6ca4bfc1e4f2c670d81409
Esteban Adrian Boffa <esteban.boffa@mercadolibre.com>
2020-07-07 22:43:16 -0300

    Fix dto

--- MercadoPagoSDK/MercadoPagoSDK/Services/Models/PXOverview.swift
+++ MercadoPagoSDK/MercadoPagoSDK/Services/Models/PXOverview.swift
@@ -9,21 +9,19 @@ import Foundation
 
 public struct PXOverview: Codable, Equatable {
 
-    let title: PXText
-    let subtitle: PXText?
+    let description: [PXText]
     let amount: PXText
-    let description: PXText?
+    let brief: [PXText]?
     let iconUrl: String?
 
     enum CodingKeys: String, CodingKey {
-        case title
-        case subtitle
-        case amount
         case description
+        case amount
+        case brief
         case iconUrl = "icon_url"
     }
 
     public static func == (lhs: PXOverview, rhs: PXOverview) -> Bool {
-        return lhs.title == rhs.title && lhs.subtitle == rhs.subtitle && lhs.amount == rhs.amount && lhs.description == rhs.description && lhs.iconUrl == rhs.iconUrl
+        return lhs.description == rhs.description && lhs.amount == rhs.amount && lhs.brief == rhs.brief && lhs.iconUrl == rhs.iconUrl
     }
 }
