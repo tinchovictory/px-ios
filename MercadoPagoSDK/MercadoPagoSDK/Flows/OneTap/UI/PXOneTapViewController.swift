7e5caad1efe0230b7b10c2a6149cb478ce7b2f90
eboffa <esteban.boffa@mercadolibre.com>
2020-07-23 12:04:19 -0300

    Fix backoffice bug (#2262)
    * Fix backoffice bug
    
    * Fix comments

--- MercadoPagoSDK/MercadoPagoSDK/Flows/OneTap/UI/PXOneTapViewController.swift
+++ MercadoPagoSDK/MercadoPagoSDK/Flows/OneTap/UI/PXOneTapViewController.swift
@@ -337,7 +337,6 @@ extension PXOneTapViewController {
     }
 
     private func getActionForModal(_ action: PXRemoteAction? = nil, isSplit: Bool = false, trackingPath: String? = nil, properties: [String: Any]? = nil) -> PXAction? {
-        let defaultTitle = "Pagar con otro medio".localized
         let nonSplitDefaultAction: () -> Void = { [weak self] in
             guard let self = self else { return }
             self.currentModal?.dismiss()
@@ -349,13 +348,12 @@ extension PXOneTapViewController {
             self.currentModal?.dismiss()
         }
 
-        let defaultAction = isSplit ? splitDefaultAction : nonSplitDefaultAction
-
         guard let action = action else {
-            return PXAction(label: defaultTitle, action: defaultAction)
+            return nil
         }
 
         guard let target = action.target else {
+            let defaultAction = isSplit ? splitDefaultAction : nonSplitDefaultAction
             return PXAction(label: action.label, action: defaultAction)
         }
 
