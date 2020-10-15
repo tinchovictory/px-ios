28c21c142a072a4570a51b2df2ce45e759c23c3f
Eric Ertl <eric.ertl@mercadolibre.com>
2020-10-15 16:46:34 -0300

    Add merchantOrderId to payment body (#2352)

--- MercadoPagoSDK/MercadoPagoSDK/Core/PXCheckoutStore+Encodable.swift
+++ MercadoPagoSDK/MercadoPagoSDK/Core/PXCheckoutStore+Encodable.swift
@@ -12,11 +12,13 @@ extension PXCheckoutStore: Encodable {
     public func encode(to encoder: Encoder) throws {
         var container = encoder.container(keyedBy: PXCheckoutStoreCodingKeys.self)
         try container.encodeIfPresent(self.checkoutPreference?.id, forKey: .checkoutPreference)
+        try container.encodeIfPresent(self.checkoutPreference?.merchantOrderId, forKey: .merchantOrderId)
         try container.encodeIfPresent(self.paymentDatas, forKey: .paymentDatas)
     }
 
     enum PXCheckoutStoreCodingKeys: String, CodingKey {
         case checkoutPreference = "pref_id"
+        case merchantOrderId = "merchant_order_id"
         case paymentDatas = "payment_data"
     }
 }
